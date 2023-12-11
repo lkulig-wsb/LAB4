8fe44c86042f3c2d8f74b98f29490f9718248f0c
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-11 19:11:46 +0100

    LAB4_7 Update1 Commit.

--- src/Film.java
+++ src/Film.java
@@ -1,11 +1,11 @@
 public class Film implements Cloneable {
 
-    //Dane składowe
+    //Dane składowe klasy Film.
     private String tytul;
     private String rezyser;
     private int rokProdukcji;
 
-    //Konstruktor
+    //Konstruktor klasy Film.
     public Film(String tytul, String rezyser, int rokProdukcji)
     {
         this.tytul = tytul;
@@ -13,28 +13,32 @@ public class Film implements Cloneable {
         this.rokProdukcji = rokProdukcji;
     }
 
-    //Przeslanianie metody to String
+    //Przeslanianie metody toString(). Metoda toString() jest tekstową reprezentacją obiektu.
     @Override
     public String toString(){
-        return "Film{"+"tytul='" + tytul +'\''
-                + ", rezyser='"+ rezyser + '\''
-                + ", rok Produkcji=" +rokProdukcji + '}';
+        return "FILM << " + '\'' + tytul +'\''
+                + ", reżyser='"+ rezyser + '\''
+                + ", rok Produkcji -> " +rokProdukcji + " >>";
     }
-    //Przesłanianie metody clone()
+    //Żeby korzystać z tej metody, klasa musi implementować interfejs Cloneable.
+    //Metoda musi też zostać przesłonięta.
+    //Metoda zwraca objekt jako typ "Object".
+    //Dodatkowo, metoda zgłasza wyjątek dlatego musimy to zadeklarować w jej strukturze.
     @Override
-    protected Object clone()throws CloneNotSupportedException{
-        return super.clone();
+    protected Film clone()throws CloneNotSupportedException{
+        return (Film) super.clone();
     }
     //Przesłanianie metody equals()
     @Override
-    public boolean equals(Object obj){
-        if(this == obj)return true;
-        if(obj == null || getClass() != obj.getClass()) return false;
+    public boolean equals(Object obj){ //argument typu Object, jest to przesłanianie metody z klasy Object.
+        if(this == obj)return true; //czy objekt i przekazany obiekt to ta sama referencja.
+        //sprawdzenie czy objekt nie jest null i czy należy do tej samej klasy co bieżący objekt.
+        if(obj == null || getClass() != obj.getClass()) return false; //rzutowanie obiektu na typ Film
         Film film = (Film) obj;
 
-        if (rokProdukcji != film.rokProdukcji) return false;
-        if (tytul != null ? !tytul.equals(film.tytul) : film.tytul != null) return false;
-        return rezyser != null ? rezyser.equals(film.rezyser) : film.rezyser == null;
+        if (rokProdukcji != film.rokProdukcji) return false; //sprawdza czy pola rokProdukcji obiektów są równe
+        if (tytul != null ? !tytul.equals(film.tytul) : film.tytul != null) return false;//Porównanie pól tytul
+        return rezyser != null ? rezyser.equals(film.rezyser) : film.rezyser == null;//Porównanie pól reżyser
     }
     //Przesłanianie  metody finalize
     @Override
@@ -51,18 +55,18 @@ public class Film implements Cloneable {
     public static void main(String[] args)
     {
 
-    //Utworzenie obiektu klasy Film
+    //Utworzenie obiektu klasy Film.
         Film f1 =new Film("Władca Pierścieni","Peter Jackson",2001);
         Film f2= new Film("Ojciec Chrzestny","Francis Ford Coppola",1972);
         Film f4= new Film("Ojciec Chrzestny","Francis Ford Coppola",1972);
 
-        //Testowanie metody toString()
+        //Testowanie metody toString().
         System.out.println("toString()");
         System.out.println("----------");
-        System.out.println(f1);
-        System.out.println(f2);
+        System.out.println(f1.toString()); //wywołanie toString().
+        System.out.println(f2); //toString() można też pominąć.
 
-        //Testowanie metody clone()
+        //Testowanie metody clone().
         System.out.println("clone()");
         System.out.println("-------");
         try{
@@ -71,7 +75,7 @@ public class Film implements Cloneable {
         }catch (CloneNotSupportedException e){
             e.printStackTrace();
         }
-        //Testowanie metody equals()
+        //Testowanie metody equals().
         System.out.println("equals() " + f1.equals(f2));
         System.out.println("equals() " + f2.equals(f4));
 
